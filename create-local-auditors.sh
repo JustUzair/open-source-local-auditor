cc6ac3ac3178928c02e93558c61f62142f9766ab
JustUzair <justuzairsaiyed@gmail.com>
2026-03-28 00:38:57 +0530

    feat: added skilled ollama auditors and supervisor

---
+++ create-local-auditors.sh
@@ -0,0 +1,6 @@
+ollama create qwen-junior-auditor -f ./Modelfiles/auditors/Modelfile.qwen-junior-auditor
+ollama create qwen-senior-auditor -f ./Modelfiles/auditors/Modelfile.qwen-senior-auditor
+ollama create glm-senior-auditor -f ./Modelfiles/auditors/Modelfile.glm-5-senior-auditor
+ollama create glm-supervisor -f ./Modelfiles/supervisor/Modelfile.glm-5-supervisor
+
+
---
+++ delete-local-auditors.sh
@@ -0,0 +1,3 @@
+ollama rm qwen-junior-auditor
+ollama rm qwen-senior-auditor
+ollama rm glm-senior-auditor
